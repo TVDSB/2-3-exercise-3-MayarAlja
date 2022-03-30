72a0ceb47903f10b886d1d105be73f08446439ef
MayarAlja <100421712+MayarAlja@users.noreply.github.com>
2022-03-30 08:29:38 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,6 +1,17 @@
 def main():
-    #your code goes here
-    print("0")
+
+  number = int(input("Enter a number here: \n"))
+
+  if number %15==0:
+    print("fizzbuzz")
+
+  elif number%5==0:
+    print("buzz")
+  elif number %3==0:
+    print("fizz")
+  else:
+    print(number)
+    
 
 if __name__ =='__main__':
     main()
